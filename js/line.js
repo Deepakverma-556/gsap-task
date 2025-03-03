a483208f079652f653641e1ecce6feab575473c2
Deepak Verma <deepakradialcode@gmail.com>
2025-03-03 10:52:09 +0530

    create line animation

--- js/line.js
+++ js/line.js
@@ -3,13 +3,58 @@ let tl = gsap.timeline({
     scrollTrigger: {
         trigger: ".parent",
         start: "top top",
-        end: "400%",
+        end: "100%",
         scrub: 2,
         pin: true,
     },
 });
-tl.to(".line",
+tl.to(".fill-line",
     {
-        bg
+        width: "25%"
     }
+)
+tl.to(".image-one",
+    {
+        opacity: 0
+    },"0.1"
+)
+tl.to(".fill-line",
+    {
+        width: "50%"
+    }
+)
+tl.to(".image-two",
+    {
+        opacity: 1
+    }, "0.5"
+)
+tl.to(".image-two",
+    {
+        opacity: 0
+    }
+)
+tl.to(".fill-line",
+    {
+        width: "75%"
+    }
+)
+tl.to(".image-three",
+    {
+        opacity: 1
+    },"1.4"
+)
+tl.to(".image-three",
+    {
+        opacity: 0
+    },"0.1"
+)
+tl.to(".fill-line",
+    {
+        width: "100%"
+    }
+)
+tl.to(".image-four",
+    {
+        opacity: 1
+    },
 )
