62fef81e22a692d10832337b7a1212f70003ac5b
Deepak Verma <deepakradialcode@gmail.com>
2025-03-03 11:40:06 +0530

    update line gsap

--- js/line.js
+++ js/line.js
@@ -7,54 +7,86 @@ let tl = gsap.timeline({
         scrub: 2,
         pin: true,
     },
-});
-tl.to(".fill-line",
-    {
-        width: "25%"
-    }
-)
-tl.to(".image-one",
-    {
-        opacity: 0
-    },"0.1"
-)
-tl.to(".fill-line",
-    {
-        width: "50%"
-    }
-)
-tl.to(".image-two",
-    {
-        opacity: 1
-    }, "0.5"
-)
-tl.to(".image-two",
-    {
-        opacity: 0
-    }
-)
-tl.to(".fill-line",
-    {
-        width: "75%"
-    }
-)
-tl.to(".image-three",
-    {
-        opacity: 1
-    },"1.4"
-)
-tl.to(".image-three",
-    {
-        opacity: 0
-    },"0.1"
-)
-tl.to(".fill-line",
-    {
-        width: "100%"
-    }
-)
-tl.to(".image-four",
-    {
-        opacity: 1
-    },
-)
+})
+tl
+    .to(".fill-line",
+        {
+            width: "16%"
+        }
+    )
+    .to(".image-one",
+        {
+            opacity: 0
+        }, "0.1"
+    )
+    .to(".fill-line",
+        {
+            width: "32%",
+            duratin:"2"
+        }
+    )
+    .to(".image-two",
+        {
+            opacity: 1
+        }, "0.5"
+    )
+    .to(".image-two",
+        {
+            opacity: 0
+        }
+    )
+    .to(".fill-line",
+        {
+            width: "48%"
+        }
+    )
+    .to(".image-three",
+        {
+            opacity: 1
+        }, "1.4"
+    )
+    .to(".image-three",
+        {
+            opacity: 0
+        }
+    )
+    .to(".fill-line",
+        {
+            width: "64%"
+        }
+    )
+    .to(".image-four",
+        {
+            opacity: 1
+        }, "2"
+    )
+    .to(".image-four",
+        {
+            opacity: 0
+        }
+    )
+    .to(".fill-line",
+        {
+            width: "80%"
+        }
+    )
+    .to(".image-five",
+        {
+            opacity: 1
+        }, "3.5"
+    )
+    .to(".image-five",
+        {
+            opacity: 0
+        }
+    )
+    .to(".fill-line",
+        {
+            width: "100%"
+        }
+    )
+    .to(".image-six",
+        {
+            opacity: 1
+        }, "4.2"
+    )
